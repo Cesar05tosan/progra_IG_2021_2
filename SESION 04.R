9740422d96490579efd0476136314a3b85807fb6
Cesar Augusto <cesar.sanchez6@unmsm.edu.pe>
2021-11-12 19:51:57 -0500

    Voy cambiando

--- SESION 04.R
+++ SESION 04.R
@@ -1,1 +1,12 @@
 ## GUI: INTERFAZ 
+
+
+clear ## limpiar git bash
+
+
+git init ### inicializar un repositorio
+git status ## saber el estado del repositoriogi
+git add ### agregar archuivos del repo
+git rm  #### quitar ar2chivo del repo
+git commit -m "mensaje"
+git log 
